567d1e7877d2016d4d8e96231cf82863f3a7a0e8
alpha <alpha@localhost.localdomain>
2020-10-18 23:23:55 +0200

    api => hackernews get links and get data from link

--- api/index.js
+++ api/index.js
@@ -2,52 +2,35 @@ const request = require("request-promise");
 const express = require("express");
 const app = express();
 const cheerio = require("cheerio");
-
 const PORT = 8080;
 
-const movies = ["https://www.rottentomatoes.com/m/the_last_full_measure"];
+// HACKERSNEWS
 
-const dataRepresent = async () => {
-  let rottenTomatoData = [];
-
-  for (let movie of movies) {
-    const response = await request({
-      uri: movie,
-      headers: {
-        accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-US,en;q=0.9,es;q=0.8",
-      },
-      gzip: true,
+// get all links from HACKERNEWS
+function getHackersNews() {
+  request.get(`https://thehackernews.com/`, function (error, response, data) {
+    let $ = cheerio.load(data);
+    let linksArray = [];
+    $(".story-link").each(function (i, elem) {
+      linksArray[i] = {
+        link: $(this).attr("href"),
+      };
     });
-
-    let $ = cheerio.load(response);
-    let title = $(
-      "h1[class='mop-ratings-wrap__title mop-ratings-wrap__title--top']"
-    )
-      .text()
-      .trim();
-    let tomatoMeterObj = $(
-      "#tomato_meter_link > .mop-ratings-wrap__percentage"
-    );
-    let tomatoMeter = tomatoMeterObj && tomatoMeterObj.text().trim();
-    let audMeterObj = $(
-      ".audience-score > .mop-ratings-wrap__score >  .articleLink  > .mop-ratings-wrap__percentage"
-    );
-    let audMeter = audMeterObj && audMeterObj.text().trim();
-    let summary = $(".mop-ratings-wrap__text").text().trim();
-
-    rottenTomatoData.push({
-      title,
-      tomatoMeter,
-      audMeter,
-      summary,
-    });
-  }
-};
-
-dataRepresent();
+    return getHackersNewsApi(linksArray);
+  });
+}
+// get one link and push data in array
+function getHackersNewsApi(linksArray) {
+  let DatasArray = [];
+  linksArray.forEach((el,index) => {
+    request.get(linksArray[index].link, function (error, response, data) {
+      let $ = cheerio.load(data);
+      DatasArray.push({'story-title':$("h1").text(), 'p':$("p").text()})
+      console.log(DatasArray, 'DatasArray')
+   });
+  });
+}
+return getHackersNews();
 
 app.listen(PORT, function () {
   console.log(`Listening on ${PORT}`);
