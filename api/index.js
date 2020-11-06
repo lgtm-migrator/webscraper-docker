99237d7896da7348f291c370055fb62e52a3030d
alpha <jc271200001@protonmail.com>
2020-11-06 19:51:03 +0100

    test docker

--- api/index.js
+++ api/index.js
@@ -10,35 +10,38 @@ app.use(cors(), bodyParser.urlencoded({ extended: true }), bodyParser.json());
 
 app.post("/api/urls", (req, res) => {
   let datasArray = [];
-  let hackernews = req.body.urls
-  // HACKERSNEWS
-  
-  // get all links from HACKERNEWS
-  function getHackersNews(hackernews) {
-    request.get(hackernews, function (error, response, data) {
-      let $ = cheerio.load(data);
-      let linksArray = [];
-      $(".story-link").each(function (i, elem) {
-        linksArray[i] = {
-          link: $(this).attr("href"),
-        };
-      });
-      return getHackersNewsApi(linksArray);
-    });
-  }
-  // push data in array to create a json and return
-  function getHackersNewsApi(linksArray) {
-    linksArray.forEach((el, index) => {
-      request.get(linksArray[index].link, function (error, response, data) {
+  let url = "";
+    for (let i = 0; i < 4; i++) {
+    url = req.body.urls[i].url;
+    // get all links
+    function getHackersNews(url) {
+      request.get(url, function (error, response, data) {
         let $ = cheerio.load(data);
-        datasArray.push({ "title": $("h1").text(), "text": $("p").text() });
-        if (datasArray.length === linksArray.length) {
-          res.status(200).send(datasArray);
-        }
+        let linksArray = [];
+        $(".story-link").each(function (i, elem) {
+          linksArray[i] = {
+            link: $(this).attr("href"),
+          };
+        });
+        return getHackersNewsApi(linksArray);
       });
-    });
+    }
+    // push data in array to create a json and return
+    function getHackersNewsApi(linksArray) {
+      linksArray.forEach((el, index) => {
+        request.get(linksArray[index].link, function (error, response, data) {
+          let $ = cheerio.load(data);
+          datasArray.push({ title: $("h1").text(), text: $("p").text() });
+          if (datasArray.length === linksArray.length) {
+            res.status(200).send(datasArray);
+          }
+        });
+      });
+    }
+    if (url !== "") {
+      getHackersNews(url);
+    }
   }
-  getHackersNews(hackernews);
 });
 
 app.listen(PORT, function () {
